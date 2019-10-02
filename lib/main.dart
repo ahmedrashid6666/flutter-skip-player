5634616a26f28f2b4a97d80ad7eb0444b79e6318
nbros <nicolas.bros@gmail.com>
2019-10-02 16:17:41 +0200

    new app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Skip Player',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Skip Player")),
+      body: Center(child: Text("Hello world", style: Theme.of(context).textTheme.display1)),
+    );
+  }
+}
